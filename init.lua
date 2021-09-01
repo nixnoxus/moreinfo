d55da23ff557d48952d339e3e5eda2fbdeb914bc
nixnoxus <nixnoxus@web.de>
2021-09-02 00:52:40 +0200

    waypoints "bones" added

--- init.lua
+++ init.lua
@@ -1,12 +1,13 @@
 -- init.lua moreinfo
 
 moreinfo =
-    { _debug = false
+    { _debug = 1
     , _experimental = false
-    -- FIXME:
+    -- FIXME: ssm vs. csm
     , text_color = ((INIT == "client") and '#8080E0' or'#E0D0A0')
     , game_info = nil
     , players_info = nil
+    , bones_limit = 3 -- FIXME: setting
     }
 
 local modname = minetest.get_current_modname()
@@ -18,6 +19,10 @@ end
 
 local function debug(msg)
     if moreinfo._debug then print(msg) end
+end
+
+local function oops(msg)
+    print("[" .. modname .. "] oops: " .. msg)
 end
 
 local function enabled(key)
@@ -104,7 +109,6 @@ local function try_keys(list, ...)
 end
 
 local huds = {}
-
 local function hud_init()
     local hud =
         { standing =
@@ -133,7 +137,7 @@ local function hud_init()
         , wp_bed_id = nil
         }
 
-    -- FIXME:
+    -- FIXME: ssm vs. csm
     if INIT == "client" then
         hud.standing.def.position.x = 1
         hud.standing.def.alignment.x = -1
@@ -175,6 +179,30 @@ local function human_s(s)
     return str
 end
 
+local wps = {}
+
+local function get_meta(meta, key, default)
+    local json = meta and meta:get(modname .. ":" .. key)
+    return json and minetest.deserialize(json) or default
+end
+
+local function set_meta(meta, key, value)
+    local rc = meta and meta:set_string(modname .. ":" .. key, minetest.serialize(value))
+end
+
+
+local function init_wp(player)
+    local player_name = get_player_name(player)
+    local meta = (INIT == "game") and player:get_meta() or nil
+
+    wps[player_name] =
+        { bone_next = get_meta(meta, "bone_next", 1)
+        , bones_pos = get_meta(meta, "bones_pos", {})
+        , bones_hid = {}
+        }
+    debug("init wp_bones next: " .. wps[player_name].bone_next)
+end
+
 local function update_game_info()
     if not enabled("display_game_info") then return end
 
@@ -199,7 +227,7 @@ local function update_players_info() -- ssm only
             local player_name = p:get_player_name()
             local player_info =  minetest.get_player_information(player_name)
             if not player_info then
-                debug("oops: nix player_info:" .. dump(player_info))
+                oops("nix player_info:" .. dump(player_info))
                 return
             end
             names[i] = player_name
@@ -297,6 +325,10 @@ local function do_hud(player)
         elseif moreinfo.game_info then
             msg = msg .. "\n"
         end
+
+        local player_name = get_player_name(player)
+        local max = #wps[player_name].bones_pos
+        msg = msg .. "bones: " .. max .. "\n"
 
         hud_show(hud.standing, player, msg .. (moreinfo.game_info or ""))
 
@@ -446,10 +478,91 @@ local function do_hud(player)
         end
 end
 
-local function died(dead_player, msg_part)
-    local pos = vector.round(dead_player:get_pos())
-    local msg = "You " .. (msg_part or "died") .. " at " .. minetest.pos_to_string(pos) .. "."
-    chat_send_player(get_player_name(dead_player), minetest.colorize(moreinfo.text_color, msg))
+local function update_wp_bones(player, player_name)
+    local max = #wps[player_name].bones_pos
+    local suffix = (INIT == "client") and "?" or ""
+
+    for i =1, max do
+        if wps[player_name].bones_hid[i] then
+            player:hud_remove(wps[player_name].bones_hid[i])
+        end
+
+        wps[player_name].bones_hid[i] = player:hud_add(
+            { hud_elem_type = "waypoint"
+            , name = "bones[" .. i .. "/" .. max .. "]" .. suffix
+            , precision = 1
+            , number = moreinfo.text_color:gsub('#', '0x')
+            , world_pos = wps[player_name].bones_pos[i]
+            }
+        )
+    end
+
+    if (INIT == "client") then return end
+    local meta = player:get_meta() or nil
+    set_meta(meta, "bones_pos", wps[player_name].bones_pos)
+    set_meta(meta, "bone_next", wps[player_name].bone_next)
+end
+
+local function check_wp_bones(player, player_name)
+    debug("checked wp_bones"
+        .. " count:" .. #wps[player_name].bones_pos
+        .. " next: " .. wps[player_name].bone_next
+        )
+    local i = 1
+    while (i <= #wps[player_name].bones_pos) do
+        local pos = wps[player_name].bones_pos[i]
+        local name = pos and minetest.get_node(pos).name
+        debug(" check bones " .. i .. " " .. vector2str(pos) .. " " .. (name or "-"))
+        if not pos or (name ~= "bones:bones" and name ~= "ignore") then
+            debug(" remove bones " .. i)
+            if wps[player_name].bones_hid[i] then
+                player:hud_remove(wps[player_name].bones_hid[i])
+            end
+            table.remove(wps[player_name].bones_pos, i)
+            table.remove(wps[player_name].bones_hid, i)
+            if wps[player_name].bone_next > i then
+                wps[player_name].bone_next = wps[player_name].bone_next -1
+            end
+        else
+            i = i +1
+        end
+    end
+    debug(" checked wp_bones"
+        .. " count:" .. #wps[player_name].bones_pos
+        .. " next: " .. wps[player_name].bone_next
+        )
+end
+
+local function died(dead_player, pos, msg_part)
+    local msg = "You " .. (msg_part or "died")
+        .. " at " .. minetest.pos_to_string(vector.round(pos)) .. "."
+    local player_name = get_player_name(dead_player)
+    chat_send_player(player_name, minetest.colorize(moreinfo.text_color, msg))
+
+    if INIT == "game" then
+        if minetest.is_creative_enabled(player_name) then
+            oops("no bones (creative)")
+            return
+        end
+        local node = minetest.get_node(pos)
+        if node.name ~= "bones:bones" then
+            oops("no bones found")
+            return
+        end
+    else
+        pos.y = pos.y + 1 -- FIXME: ssm vs. csm
+    end
+
+    local i = ((wps[player_name].bone_next -1) % moreinfo.bones_limit) +1
+    if i > #wps[player_name].bones_pos +1 then
+        i = #wps[player_name].bones_pos +1
+    end
+
+    debug("add bones " .. i);
+    wps[player_name].bones_pos[i] = pos
+    wps[player_name].bone_next = i + 1
+
+    update_wp_bones(dead_player, player_name)
 end
 
 local function get_description(name)
@@ -489,7 +602,7 @@ local function update_wp_bed(player)
     if spawn then
         debug(" spawn " .. dump(spawn))
         if not huds[player_name].wp_bed_id then
-            debug(" add wp")
+            debug(" add wp bed")
             huds[player_name].wp_bed_id = player:hud_add(
                 { hud_elem_type = "waypoint"
                 , name = "spawn (bed)"
@@ -537,11 +650,12 @@ if INIT == "client" then
     local player = minetest.localplayer
     debug(" localplayer:"..dump(player))
     huds[get_player_name(player)] = hud_init()
-
+    init_wp(player)
     -- minetest.ui.minimap:show()
 
     minetest.register_on_death(function()
-	died(minetest.localplayer)
+        local player = minetest.localplayer
+	died(player, player:get_pos())
     end)
 
     minetest.register_globalstep(function(dtime)
@@ -585,11 +699,31 @@ elseif INIT == "game" then
         update_wp_bed = function(...) return nil end
     end
 
-    if enabled("public_death_messages") then
+    if minetest.get_modpath("bones") then
+        local name = "bones:bones"
+        local orig_on_punch = minetest.registered_nodes[name].on_punch
+
+        minetest.override_item(name, {
+          on_punch = function(pos, node, player)
+            debug("running bones hook " .. minetest.get_node(pos).name )
+            orig_on_punch(pos, node, player)
+
+            local players = minetest.get_connected_players()
+            table.foreach(players, function(_,p)
+                local n = get_player_name(p)
+                check_wp_bones(p, n)
+                update_wp_bones(p, n)
+            end)
+        end })
+    end
+
+    minetest.register_on_dieplayer(function(dead_player, reason)
+        local dead_player_name = dead_player:get_player_name()
+        local pos = dead_player:get_pos()
+
+        if enabled("public_death_messages") then
         -- Note: chat message "You died." already removed from core
         --  see: https://github.com/minetest/minetest/pull/11443/files
-        minetest.register_on_dieplayer(function(dead_player, reason)
-            local dead_player_name = dead_player:get_player_name()
             local msg_part
             debug("dead reason:" .. dump(reason))
             if reason.type == "punch" then
@@ -607,16 +741,16 @@ elseif INIT == "game" then
             -- `minetest.chat_send_all(text)
             for _, player in pairs(minetest.get_connected_players()) do
                 if (player == dead_player) then
-                    died(dead_player, msg_part or "died")
+                    died(dead_player, pos, msg_part or "died")
                 else
                     chat_send_player(player:get_player_name()
                         , dead_player_name .. " " .. (msg_part or "died")
                         )
                 end
             end
-        end)
+        end
+    end)
 
-    end
 
 --[[ minetest.conf: chat_message_format = @timestamp @name: @message
 
@@ -639,6 +773,9 @@ elseif INIT == "game" then
             )
         dump("server_status:".. dump(minetest.get_server_status()))
 
+        init_wp(player)
+        check_wp_bones(player, player_name)
+        update_wp_bones(player, player_name)
         update_wp_bed(player)
 --        player:hud_set_flags({ minimap = true, minimap_radar =  true })
     end)
